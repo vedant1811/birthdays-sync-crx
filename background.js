c8d2998dbe06304e25dd8f3d080f276ff2a9db02
Vedant Agarwala <mail@vedant-agarwala.com>
2018-11-16 21:18:42 +0530

    Open the facebook birthdays tab on click

--- background.js
+++ background.js
@@ -1,5 +1,25 @@
+const BIRTHDAYS_URL = "https://www.facebook.com/events/birthdays/"
+
 console.log("yay 1");
 
+function main() {
+  console.log("yay");
+
+  chrome.tabs.create({
+    url: BIRTHDAYS_URL,
+    active: false
+  }, function (birthdaysTab) {
+    chrome.tabs.executeScript(birthdaysTab.id)
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+      console.log("yay 3");
+      // make sure the status is 'complete' and it's the right tab
+      if (tab.id == birthdaysTab.id && changeInfo.status == 'complete') {
+        console.log("yay 2");
+      }
+    });
+  });
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, {file: "main.js"});
+  main();
 });
