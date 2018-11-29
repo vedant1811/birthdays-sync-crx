11313efc71a255d7294027a40c55d6dcfd922ee9
Vedant Agarwala <mail@vedant-agarwala.com>
2018-11-29 18:07:53 +0530

    Extracting today's birthdays from facebook page

--- background.js
+++ background.js
@@ -1,22 +1,18 @@
 const BIRTHDAYS_URL = "https://www.facebook.com/events/birthdays/"
 
-console.log("yay 1");
-
 function main() {
-  console.log("yay");
 
   chrome.tabs.create({
     url: BIRTHDAYS_URL,
     active: false
   }, function (birthdaysTab) {
-    chrome.tabs.executeScript(birthdaysTab.id)
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-      console.log("yay 3");
-      // make sure the status is 'complete' and it's the right tab
-      if (tab.id == birthdaysTab.id && changeInfo.status == 'complete') {
-        console.log("yay 2");
+    chrome.tabs.executeScript(
+      birthdaysTab.id,
+      { file: 'onFacebookPage.js' },
+      function callback(resultsArray) {
+        console.log(resultsArray);
       }
-    });
+    );
   });
 }
 
--- onFacebookPage.js
+++ onFacebookPage.js
@@ -1,1 +1,45 @@
-console.log('yay fb');
+/*
+Birthday with age HTML structure:
+  #birthdays_content
+    #birthdays_today_card
+    div
+      ul
+        li
+          ...
+            div
+              div
+                a {name}
+              div {age}
+
+
+Birthday without age HTML structure:
+  #birthdays_content
+    #birthdays_today_card
+    div
+      ul
+        li
+          ...
+            div
+              a {name}
+            form
+
+*/
+
+
+(function todaysBirthdays() {
+  console.log('todaysBirthdays');
+
+  var hash = {};
+
+  var list = birthdays_today_card.nextSibling.querySelector('ul').children;
+
+  for (listItem of list) {
+    var profileAnchor = listItem.querySelector('a');
+    var name = profileAnchor.textContent;
+    var ageString = profileAnchor.parentNode.nextSibling.textContent; // can be blank
+    console.log(name + " is " + ageString);
+    hash[name] = ageString;
+  }
+
+  return hash;
+}) ();
